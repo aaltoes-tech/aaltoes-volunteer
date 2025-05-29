50849fe314c72fe7eb95ffc1537a7f142d5b617e
Yerzhan Zhamashev <yerzhan@novel.systems>
2025-05-29 17:06:22 +0300

    Experimental linear issues list

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { data } from "react-router";
+import { linearService } from "~/lib/linear";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +17,46 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function loader({}: Route.LoaderArgs) {
+  const linearClient = await linearService.getLinearClient();
+  if (!linearClient) {
+    return data({
+      issues: [],
+    });
+  }
+  const issues = await linearClient.issues({
+    filter: {
+      assignee: {
+        app: {
+          eq: true,
+        },
+      },
+    },
+  });
+  return data({
+    issues: issues.nodes,
+  });
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { issues } = loaderData;
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {issues.map((issue) => (
+            <TableRow key={issue.id}>
+              <TableCell>{issue.title}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
