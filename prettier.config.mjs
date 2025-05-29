3c2d83e75bec1a71450d4d913e991d7aefe47d0a
Yerzhan Zhamashev <yerzhan@novel.systems>
2025-05-29 20:50:59 +0300

    Add Prettier and related plugins for code formatting
    - Introduced Prettier as a dependency for code formatting.
    - Added Prettier plugins: prettier-plugin-sort-imports and prettier-plugin-tailwindcss for enhanced formatting capabilities.
    - Updated package.json scripts to include formatting commands for code quality checks.
    - Modified pnpm-lock.yaml to reflect new dependencies and their versions.

---
+++ prettier.config.mjs
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+
+/** @type { import("prettier").Config | import("prettier-plugin-tailwindcss").PluginOptions | import("@ianvs/prettier-plugin-sort-imports").PluginConfig } */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindConfig: fileURLToPath(
+    new URL("../../config/tailwind/web.ts", import.meta.url),
+  ),
+  tailwindFunctions: ["cn", "cva"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@junction",
+    "^@junction/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.8.3",
+};
+
+export default config;
